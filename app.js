2220b2c383c8dbf99371a65957483ae9908ebc57
LeonelAV <meirinho_leonel@hotmail.com>
2017-10-08 09:40:52 +0100

    structure changes

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var userInfo = function(){
-  console.log('button was clicked')
+  //console.log('button was clicked')
   var username = document.getElementById('username').value
 
   var userData = function(url, callback){
 
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState == 4 && xhr.status == 200){ //readyState 4 means the request is done and status 200 is a successful return
-      callback(xhr)
-  } else {
-      callback(xhr.status) //an error ocurred during the request
+    var xhr = new XMLHttpRequest()
+
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState == 4 && xhr.status == 200){ //readyState 4 means the request is done and status 200 is a successful return
+        callback(xhr)
+      } else {
+        callback(xhr.status) //an error ocurred during the request
+      }
   }
-}
+
   xhr.open('GET', url, true)
   xhr.responseType = 'json'
   xhr.send()
@@ -21,6 +23,7 @@ var userInfo = function(){
   userData('https://api.github.com/users/' + username, function(xhr){
     console.log(xhr.response)
   })
+
   userData('https://api.github.com/users/' + username + '/repos', function(xhr){
     console.log(xhr.response)
   })
