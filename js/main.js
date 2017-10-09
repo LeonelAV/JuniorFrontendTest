4e0c356acdb4c6378501ef786dc4b0899df4e16f
LeonelAV <meirinho_leonel@hotmail.com>
2017-10-09 11:47:16 +0100

    error created

--- js/main.js
+++ js/main.js
@@ -1,56 +1,60 @@
-var ourButton = document.getElementById('our-button') //wrap the button element in a variable
+var ourButton = document.querySelector('#our-button') //wrap the button element in a variable
 
 var userInfo = function(){
   //console.log('button was clicked')
-  var username = document.getElementById('username').value
+  var username = document.querySelector('#username').value
 
   var userData = function(url, callback){
     var xhr = new XMLHttpRequest()
     xhr.onload = function(){
-      if(xhr.readyState == 4 && xhr.status == 200){ //readyState 4 means the request is done and status 200 is a successful return
+      if (xhr.readyState == 4 && xhr.status == 200){ //readyState 4 means the request is done and status 200 is a successful return
         callback(null, xhr.response)
       } else {
         callback(xhr.status) //an error ocurred during the request
       }
   }
-
-  xhr.open('GET', url, true)
-  xhr.responseType = 'json'
-  xhr.send()
+    xhr.open('GET', url, true)
+    xhr.responseType = 'json'
+    xhr.send()
 }
 
 
-  userData('https://api.github.com/users/' + username, function(err, data){
-    if(err != null ) {
-      document.getElementById('error').style.display = 'inline-block'
-      document.getElementById('user-info').style.display = 'none'
-      document.getElementById('repositories-info').style.display = 'none'
-  	} else {
-      console.log(data)
-      document.getElementById('user-info').style.display = 'inline-flex'
-      document.getElementById('repositories-info').style.display = 'initial'
-      document.getElementById('error').style.display = 'none'
-      document.getElementById('user-photo').style.display = 'inline-block'
-      document.getElementById('user-photo').src = data.avatar_url
-      document.getElementById('name').innerHTML = '@' + data.login
-      document.getElementById('fullname').innerHTML = data.name
-      document.getElementById('bio').innerHTML = data.bio
+  userData('https://api.github.com/users/' + username, function(err, data) {
+    if (err != null) {
+      const div = document.createElement('DIV')
+      const pError = document.createElement('P')
+      pError.textContent = "Does not exist"
+      div.className = 'error'
+      div.appendChild(pError)
+      const container = document.querySelector('.main-container')
+      container.appendChild(div)
+      document.querySelector('.error').style.display = 'inline-block'
+      document.querySelector('#user-info').style.display = 'none'
+      document.querySelector('#repositories-info').style.display = 'none'
+    } else {
+      //console.log(data)
+      document.querySelector('#user-info').style.display = 'inline-flex'
+      document.querySelector('#repositories-info').style.display = 'initial'
+      document.querySelector('#user-photo').style.display = 'inline-block'
+      document.querySelector('#user-photo').src = data.avatar_url
+      document.querySelector('#name').innerHTML = '@' + data.login
+      document.querySelector('#fullname').innerHTML = data.name
+      document.querySelector('#bio').innerHTML = data.bio
+      document.querySelector('.error').style.display = 'none'
     }
   })
-
-
 
   userData('https://api.github.com/users/' + username + '/repos', function(err, data){
     if (err != null ) {
       throw err
-  	} else {
-      document.getElementById('repositories-list').style.display = 'inline-block'
-      var reposHtml = document.getElementById('repository-details')
+    } else {
+      document.querySelector('#repositories-list').style.display = 'inline-block'
+      var reposHtml = document.querySelector('#repository-details')
       var tpl = ''
       data.forEach(function(elem){
         tpl += '<li><h4>' + elem.name + '</h4>' 
             + '<div><img src="images/filled_star1600.png">' 
-            + elem.stargazers_count + '<img src="images/logo.png">' 
+            + elem.stargazers_count + '<img src="images/logo.png">'
             + elem.forks_count + '</div</li>'
       })
       reposHtml.innerHTML = tpl
@@ -58,4 +62,4 @@ var userInfo = function(){
   })
 }
 
-ourButton.addEventListener('click', userInfo) 
+ourButton.addEventListener('click', userInfo)
